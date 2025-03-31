49b15fe647ea665654a2e984e6db3f25014a863f
mazurmilosz000 <mazur.milosz000@gmail.com>
2025-03-31 18:28:42 +0200

    get rid of code duplicates in category controller

--- server/commons/constants.go
+++ server/commons/constants.go
@@ -1,9 +1,10 @@
 package commons
 
 const (
-	DatabaseError        = "Database error"
-	CartNotFoundError    = "Cart not found"
-	InvalidIdError       = "Invalid ID"
-	InvalidRequestError  = "Invalid request payload"
-	ProductNotFoundError = "Product not found"
+	DatabaseError         = "Database error"
+	CartNotFoundError     = "Cart not found"
+	InvalidIdError        = "Invalid ID"
+	InvalidRequestError   = "Invalid request payload"
+	ProductNotFoundError  = "Product not found"
+	CategoryNotFoundError = "Category not found"
 )
--- server/controllers/categoryController.go
+++ server/controllers/categoryController.go
@@ -7,6 +7,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 
+	"api-project/commons"
 	"api-project/database"
 	"api-project/models"
 )
@@ -17,19 +18,18 @@ func GetAllCategories(c echo.Context) error {
 	return c.JSON(http.StatusOK, categories)
 }
 
-
 func GetCategoryById(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid ID"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": commons.InvalidIdError})
 	}
 
 	var category models.Category
 	if err := database.DB.Preload("Products").First(&category, id).Error; err != nil {
 		if err == gorm.ErrRecordNotFound {
-			return c.JSON(http.StatusNotFound, map[string]string{"error": "Category not found"})
+			return c.JSON(http.StatusNotFound, map[string]string{"error": commons.CategoryNotFoundError})
 		}
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Database error"})
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": commons.DatabaseError})
 	}
 
 	return c.JSON(http.StatusOK, category)
@@ -38,7 +38,7 @@ func GetCategoryById(c echo.Context) error {
 func CreateCategory(c echo.Context) error {
 	category := new(models.Category)
 	if err := c.Bind(category); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": commons.InvalidRequestError})
 	}
 	database.DB.Create(category)
 	return c.JSON(http.StatusCreated, category)
@@ -47,16 +47,14 @@ func CreateCategory(c echo.Context) error {
 func DeleteCategory(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid ID"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": commons.InvalidIdError})
 	}
 
-	var category models.Category
-	if err := database.DB.First(&category, id).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return c.JSON(http.StatusNotFound, map[string]string{"error": "Category not found"})
-		}
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Database error"})
+	category, status, errResponse := findCategoryByID(id)
+	if category == nil {
+		return c.JSON(status, errResponse)
 	}
+
 	database.DB.Delete(&category)
 	return c.JSON(http.StatusOK, map[string]string{"message": "Category deleted successfully"})
 }
@@ -64,21 +62,29 @@ func DeleteCategory(c echo.Context) error {
 func UpdateCategoryById(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid ID"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": commons.InvalidIdError})
 	}
 
-	var category models.Category
-	if err := database.DB.First(&category, id).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
-			return c.JSON(http.StatusNotFound, map[string]string{"error": "Category not found"})
-		}
-		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Database error"})
+	category, status, errResponse := findCategoryByID(id)
+	if category == nil {
+		return c.JSON(status, errResponse)
 	}
 
 	updatedCategory := new(models.Category)
 	if err := c.Bind(updatedCategory); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request payload"})
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": commons.InvalidRequestError})
 	}
 	database.DB.Model(&category).Updates(updatedCategory)
 	return c.JSON(http.StatusOK, category)
+}
+
+func findCategoryByID(id int) (*models.Category, int, map[string]string) {
+	var category models.Category
+	if err := database.DB.First(&category, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, http.StatusNotFound, map[string]string{"error": commons.CategoryNotFoundError}
+		}
+		return nil, http.StatusInternalServerError, map[string]string{"error": commons.DatabaseError}
+	}
+	return &category, http.StatusOK, nil
 }
